34caf640278f25ac653baec3e0cf381dece32866
Subash Rijal <subashrijal5@gmail.com>
2022-05-24 23:24:09 +0545

    paypal and stripe file added

---
+++ src/Gateways/PaymentGatewayInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Subash\Payment\Gateways;
+
+interface PaymentGatewayInterface
+{
+    public function request($parameters);
+
+    public function send();
+
+    public function response($request);
+
+    public function getOrderDetails($parameters);
+}
